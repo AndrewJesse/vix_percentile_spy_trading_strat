3094278298fa70fc52c3a0282a86430551dc7698
A.J. Marchese <aj.marchese@outlook.com>
2023-10-22 17:07:26 -0700

    upgraded percentile plotter

--- gold_price.py
+++ gold_price.py
@@ -1,82 +1,112 @@
 import json
-import quandl
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+import quandl
 
-# Load API key from JSON file
+# User-configurable settings
+VIX_THRESHOLD = 50
+LOOKBACK_DAYS = 20
+
+# Load API keys from JSON file
 with open("config.json", "r") as file:
     config = json.load(file)
+    fred_api_key = config["fred_api_key"]
     quandl_api_key = config["quandl_api_key"]
 
-# Set up Quandl API Key
 quandl.ApiConfig.api_key = quandl_api_key
 
-# Fetch LBMA Gold Price Data
-data = quandl.get("LBMA/GOLD")
 
-# Filter data to only include the last 3 years
-end_date = pd.Timestamp.now()
-start_date = end_date - pd.DateOffset(years=10)
-filtered_data = data[start_date:end_date]
+# Function to get data from FRED
+def fetch_fred_data(series_id, api_key):
+    url = f"https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json"
+    response = requests.get(url)
+    data = response.json()["observations"]
+    df = pd.DataFrame(data)
+    df["date"] = pd.to_datetime(df["date"])
+    df.set_index("date", inplace=True)
+    df["value"] = pd.to_numeric(df["value"], errors="coerce")
+    return df
 
-# Initialize total P/L
-total_pl_percentage = 0
+
+# Fetch VIX data from FRED
+vix_data = fetch_fred_data("VIXCLS", fred_api_key)
+
+# Fetch LBMA Gold Price Data from Quandl
+gold_data = quandl.get("LBMA/GOLD")["USD (AM)"]
+
+# Calculate the VIX percentiles
+vix_data["Percentile"] = vix_data["value"].rank(pct=True) * 100
+vix_data["Rolling Percentile"] = (
+    vix_data["value"]
+    .rolling(window=LOOKBACK_DAYS)
+    .apply(lambda x: pd.Series(x).rank(pct=True).iloc[-1] * 100)
+)
+vix_data["Rolling Percentile"].fillna(vix_data["Percentile"], inplace=True)
+
+# Align gold data with VIX data
+aligned_data = pd.concat([gold_data, vix_data["Rolling Percentile"]], axis=1).dropna()
+
+# Calculate P/L% for the entire duration
+long_term_entry = aligned_data["USD (AM)"].iloc[0]
+long_term_exit = aligned_data["USD (AM)"].iloc[-1]
+long_term_pl_percentage = ((long_term_exit - long_term_entry) / long_term_entry) * 100
+
+# Calculate P/L% for the strategy
+strategy_data = aligned_data[aligned_data["Rolling Percentile"] > VIX_THRESHOLD]
+strategy_pl_percentage = (
+    (strategy_data["USD (AM)"].iloc[-1] - strategy_data["USD (AM)"].iloc[0])
+    / strategy_data["USD (AM)"].iloc[0]
+) * 100
 
 # Plotting
 plt.figure(figsize=(14, 7))
-plt.plot(filtered_data["USD (AM)"], label="AM Price", color="blue")
-# plt.plot(filtered_data["USD (PM)"], label="PM Price", linestyle="--", color="cyan")
+plt.plot(aligned_data.index, aligned_data["USD (AM)"], linestyle="-", color="red")
 
-# Iterate over each year and simulate the trade
-for year in range(start_date.year, end_date.year + 1):
-    trade_entry_date = pd.Timestamp(year=year, month=1, day=2)
-    trade_exit_date = trade_entry_date + pd.DateOffset(months=3)
+is_above_threshold = aligned_data["Rolling Percentile"] > VIX_THRESHOLD
+start_date = None
+for date, above in is_above_threshold.items():
+    if above and start_date is None:
+        start_date = date
+    elif not above and start_date:
+        plt.plot(
+            aligned_data[start_date:date].index,
+            aligned_data[start_date:date]["USD (AM)"],
+            marker="o",
+            markersize=1,
+            color="blue",
+            linestyle="-",
+        )
+        start_date = None
+if start_date:
+    plt.plot(
+        aligned_data[start_date:].index,
+        aligned_data[start_date:]["USD (AM)"],
+        marker="o",
+        markersize=1,
+        color="blue",
+        linestyle="-",
+    )
 
-    # Adjust trade dates to nearest available data points
-    if trade_entry_date not in filtered_data.index:
-        trade_entry_date = filtered_data.index[filtered_data.index > trade_entry_date][
-            0
-        ]
-    if trade_exit_date not in filtered_data.index:
-        trade_exit_date = filtered_data.index[filtered_data.index > trade_exit_date][0]
-
-    # Get the segment of data corresponding to the trade
-    trade_data_segment = filtered_data[trade_entry_date:trade_exit_date]["USD (AM)"]
-
-    entry_price = trade_data_segment.iloc[0]
-    exit_price = trade_data_segment.iloc[-1]
-    pl_percentage = ((exit_price - entry_price) / entry_price) * 100
-    total_pl_percentage += pl_percentage
-
-    # Plot the segment corresponding to the trade
-    plt.plot(trade_data_segment, color="red")
-
-# Calculate P/L for holding from the start to the end
-long_term_entry = filtered_data["USD (AM)"].iloc[0]
-long_term_exit = filtered_data["USD (AM)"].iloc[-1]
-long_term_pl_percentage = ((long_term_exit - long_term_entry) / long_term_entry) * 100
-
-plt.title("LBMA Gold Prices (Last 3 Years) with Simulated Trades")
-plt.xlabel("Date")
-plt.ylabel("Price in USD")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-
-# Displaying total P/L and long-term P/L
+# Annotations
 plt.annotate(
-    f"Total P/L from Trades: {total_pl_percentage:.2f}%",
-    (0.6, 0.05),
+    f"Total P/L from Strategy: {strategy_pl_percentage:.2f}%",
+    (0.02, 0.95),
     xycoords="axes fraction",
     fontsize=10,
-    color="red",
+    color="blue",
 )
 plt.annotate(
     f"P/L from Long-Term Holding: {long_term_pl_percentage:.2f}%",
-    (0.6, 0.02),
+    (0.02, 0.90),
     xycoords="axes fraction",
     fontsize=10,
     color="green",
 )
 
+plt.title("Gold Prices with Color Change based on VIX Percentile")
+plt.xlabel("Date")
+plt.ylabel("Gold Price in USD")
+plt.grid(True)
+plt.tight_layout()
 plt.show()
--- vix_percentile.py
+++ vix_percentile.py
@@ -2,49 +2,58 @@ import json
 from fredapi import Fred
 import matplotlib.pyplot as plt
 
+# Global Variables
+CONFIG_PATH = "config.json"
+WINDOW_SIZE = 50
+PERCENTILE_THRESHOLD = 20  # The user can adjust this value
+
 # Load API keys from JSON file
-with open("config.json", "r") as file:
+with open(CONFIG_PATH, "r") as file:
     config = json.load(file)
-    fred_api_key = config["fred_api_key"]
+    FRED_API_KEY = config["fred_api_key"]
 
-# Set up FRED API Key
-fred = Fred(api_key=fred_api_key)
+fred = Fred(api_key=FRED_API_KEY)
 
-# Fetch VIX data from FRED
-vix_data = fred.get_series("VIXCLS")
 
-# Calculate the percentile of each day's VIX value over a rolling window of 35 days
-vix_percentile = vix_data.rolling(window=35).apply(
-    lambda x: (x.rank(pct=True).iloc[-1]) * 100
-)
-# Fill gaps using interpolation
-vix_data_filled = vix_data.interpolate(method="linear")
+def fetch_vix_data():
+    return fred.get_series("VIXCLS")
 
-# Calculate the percentile of each day's VIX value over a rolling window of 35 days
-vix_percentile = vix_data_filled.rolling(window=35).apply(
-    lambda x: (x.rank(pct=True).iloc[-1]) * 100
-)
 
-# Calculate the percentage of time VIX percentile is below 50%
-below_50_percent = (vix_percentile < 50).sum() / len(vix_percentile) * 100
+def calculate_vix_percentile(vix_data):
+    # Fill gaps using interpolation
+    vix_data_filled = vix_data.interpolate(method="linear")
+    # Calculate the percentile of each day's VIX value over a rolling window
+    return vix_data_filled.rolling(window=WINDOW_SIZE).apply(
+        lambda x: (x.rank(pct=True).iloc[-1]) * 100
+    )
 
-# Plotting the percentile data
-plt.figure(figsize=(14, 7))
-plt.plot(vix_percentile, label="VIX 35-Day Percentile")
-plt.title("VIX 35-Day Percentile")
-plt.xlabel("Date")
-plt.ylabel("Percentile")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
 
-# Annotate the percentage of time VIX percentile is below 50%
-plt.annotate(
-    f"VIX Percentile is below 50% for {below_50_percent:.2f}% of the time",
-    (0.05, 0.05),
-    xycoords="axes fraction",
-    fontsize=10,
-    color="red",
-)
+def plot_vix_percentile(vix_percentile):
+    plt.figure(figsize=(14, 7))
+    plt.plot(vix_percentile, label=f"VIX {WINDOW_SIZE}-Day Percentile")
+    plt.title(f"VIX {WINDOW_SIZE}-Day Percentile")
+    plt.xlabel("Date")
+    plt.ylabel("Percentile")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
 
-plt.show()
+    # Annotate the percentage of time VIX percentile is below PERCENTILE_THRESHOLD
+    below_threshold_percent = (
+        (vix_percentile < PERCENTILE_THRESHOLD).sum() / len(vix_percentile) * 100
+    )
+    plt.annotate(
+        f"VIX Percentile is below {PERCENTILE_THRESHOLD}% for {below_threshold_percent:.2f}% of the time",
+        (0.05, 0.05),
+        xycoords="axes fraction",
+        fontsize=10,
+        color="red",
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    vix_data = fetch_vix_data()
+    vix_percentile = calculate_vix_percentile(vix_data)
+    plot_vix_percentile(vix_percentile)
